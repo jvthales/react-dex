a9156ee9967f286809242a164f61ee98eed31de6
unknown <jvthales@gmail.com>
2017-07-23 17:22:55 -0300

    first commit

---
+++ exercise-01-solution/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pokedex from './components/Pokedex'
+import { Router, Route, browserHistory } from 'react-router'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import 'tachyons'
+import './index.css'
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj5h4ak0g057m0122qkp802iy'}),
+})
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={Pokedex} />
+    </Router>
+  </ApolloProvider>
+  ),
+  document.getElementById('root')
+)
